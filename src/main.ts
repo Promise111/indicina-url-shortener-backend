88f17d4e2a94ce83cbeda1c7c9c81f5c556482ab
Ihunna Promise <pihunna@yahoo.com>
2025-05-01 20:43:05 +0100

    feat: add swagger api docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import config from './config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Logger } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +14,37 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  if (config.nodeEnv != 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('URL Shortener - Indicina')
+      .setDescription('Indicina URL Shortener API')
+      .setVersion('1.0')
+      .addTag('Indicina')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
+  }
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    credentials: true,
+  });
   await app.listen(config.port);
 }
-bootstrap();
+bootstrap()
+  .then(() => {
+    Logger.log(`
+    ------------
+    Server Application Started!
+    API V1: http://localhost:${config.port}
+    API Docs: http://localhost:${config.port}/docs
+    Microserservice Started Successfully
+    ------------
+`);
+  })
+  .catch((error) => {
+    Logger.error(`Failed to start the application:= , ${error}`);
+  });
